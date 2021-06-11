3fb4dc62673c20e9f46bcb5ebed05b6aafd715db
MarshRabbit <jinbh09@gmail.com>
2021-06-11 10:59:43 +0900

    Modify

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,9 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        SimulatedAnnealing sa = new SimulatedAnnealing();
+        SimulatedAnnealing sa_q = new SimulatedAnnealing(1000);
+        SimulatedAnnealing sa_c = new SimulatedAnnealing(1000);
+
         Problem p_quartic = new Problem() {
             @Override
             public double fit(double x) {
@@ -23,18 +25,18 @@ public class Main {
 
             @Override
             public boolean isNeighborBetter(double f0, double f1) {
-                return f0 > f1;
+                return f0 > f1; // 극소값
             }
         };
 
-        double x1 = sa.solve(p_quartic, 1000, 0.95,-1, -5, 5);
-        double x2 = sa.solve(p_cubic, 1000, 0.99, -7,6);
+        double x1 = sa_q.solve(p_quartic, 1000, 0.95,-1, -5, 5);
+        double x2 = sa_c.solve(p_cubic, 1000, 0.99, -7,6);
 
         System.out.println(x1);
-        System.out.println(p_quartic.fit(x1)+"\n");
+        System.out.println(p_quartic.fit(x1));
+       // System.out.println(sa_q.hist_x+"\n");
 
         System.out.println(x2);
         System.out.println(p_cubic.fit(x2));
-        //System.out.println(sa.hist);
     }
 }
--- src/com/company/SimulatedAnnealing.java
+++ src/com/company/SimulatedAnnealing.java
@@ -5,9 +5,13 @@ import java.util.Random;
 
 public class SimulatedAnnealing {
     public ArrayList<Double> hist;
+    public ArrayList<Double> hist_x;
+    private int niter;
 
-    public SimulatedAnnealing() {
+    public SimulatedAnnealing(int niter) {
+        this.niter = niter;
         hist = new ArrayList<>();
+        hist_x = new ArrayList<>();
     }
 
     public double solve(Problem p, double t, double a, double lower, double upper) {
@@ -19,18 +23,20 @@ public class SimulatedAnnealing {
     // 온도, 냉각률, 초기값, 해의 범위의 최소, 최대
     public double solve(Problem p, double t, double a, double x0, double lower, double upper) {
         Random r = new Random();
-        double f0 = p.fit(x0);
+        double f0 = p.fit(x0); // 함수
+        hist_x.add(x0);
         hist.add(f0);
 
-        while (t > 0.01){
+        for (int i=0; i<niter; i++) {
             int kt = (int) t;
             for(int j=0; j<kt; j++) {
                 double x1 = r.nextDouble() * (upper - lower) + lower; // 이웃해
                 double f1 = p.fit(x1); // 이웃해의 함수값
 
-                if(p.isNeighborBetter(f0, f1)) {
+                if(p.isNeighborBetter(f0, f1)) { // 이웃해가 더 좋은경
                     x0 = x1;
                     f0 = f1;
+                    hist_x.add(x0);
                     hist.add(f0);
                 } else {
                     double d = Math.sqrt(Math.abs(f1 - f0));
@@ -38,6 +44,7 @@ public class SimulatedAnnealing {
                     if(r.nextDouble() < p0) {
                         x0 = x1;
                         f0 = f1;
+                        hist_x.add(x0);
                         hist.add(f0);
                     }
                 }
